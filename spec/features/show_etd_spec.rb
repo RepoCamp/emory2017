a75fb0f99eb2cd65f677c5b3cbd8854c0214d80f
Bess Sadler <bess@curationexperts.com>
2017-04-14 17:34:45 -0400

    Working ETD show feature

---
+++ spec/features/show_etd_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature 'Display an ETD' do
+  let(:title) { ['China and its Minority Population'] }
+  let(:creator) { ['Eun, Dongwon'] }
+  let(:keyword) { ['China', 'Minority Population'] }
+  let(:visibility) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+  let :etd do
+    Etd.new(title: title, creator: creator, keyword: keyword, visibility: visibility)
+  end
+
+  context 'a logged in user' do
+    let(:user_attributes) do
+      { email: 'test@example.com' }
+    end
+    let(:user) do
+      User.new(user_attributes) { |u| u.save(validate: false) }
+    end
+
+    before do
+      login_as user
+      etd.save
+    end
+
+    scenario "Show an ETD" do
+      visit("/concern/etds/#{etd.id}")
+      expect(page).to have_content etd.title.first
+      expect(page).to have_content etd.creator.first
+      expect(page).to have_content etd.keyword.first
+    end
+  end
+end
